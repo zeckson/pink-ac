1d5b5e97884a48eb283965e1e62e893905c74f96
KirillGrant <grant323.43@gmail.com>
2020-05-11 22:57:54 +0300

    gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
-var gulp=require('gulp');
-var sass=require('gulp-sass');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
-    return gulp.src('app/sass/**/*.sass')
+    return gulp.src('app/sass/**/*.+(scss|sass)')
         .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+    })
+})
+
+
+
 gulp.task('sass:watch', function () {
-    gulp.watch('app/sass/**/*.sass', gulp.parallel('sass'))
+    gulp.watch('app/sass/**/*.+(scss|sass)', gulp.task('sass'))
 });
+
+gulp.task('start', gulp.parallel('sass', 'browserSync', 'sass:watch'))
