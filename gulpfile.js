eca959d814f57f3b42ac5601cbbc063669808ae7
KirillGrant <grant323.43@gmail.com>
2020-05-16 00:48:57 +0300

    1st day

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var del = require('del');
+var concat = require('gulp-concat');
 
 gulp.task('sass', function () {
     return gulp.src('app/sass/**/*.+(scss|sass)')
         .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('app/css'))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('app/css/'))
         .pipe(browserSync.reload({
             stream: true
         }))
 });
+
 
 gulp.task('clean', function () {
     return del(['app/css']);
